c36c8ced0a569e467cdc4bb90699995136085b41
mymuseisyou <daydreaming0721@gmail.com>
2024-03-31 18:28:02 +0900

    header

--- main/views.py
+++ main/views.py
@@ -17,8 +17,9 @@ def community(request):
 def posting(request, pk):
     # 게시글(Post) 중 pk(primary_key)를 이용해 하나의 게시글(post)를 검색
     post = Post.objects.get(pk=pk)
+    postlist = Post.objects.all()
     # posting.html 페이지를 열 때, 찾아낸 게시글(post)을 post라는 이름으로 가져옴
-    return render(request, 'main/posting.html', {'post':post})
+    return render(request, 'main/posting.html', {'post':post, 'postlist':postlist})
 
 
 def newpost(request):
